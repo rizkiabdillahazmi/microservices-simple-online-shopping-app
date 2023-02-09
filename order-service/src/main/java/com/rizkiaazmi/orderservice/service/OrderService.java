8293ce52159acd0e67ca155051776ac581f6de83
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-02-09 13:02:57 +0700

    basic setup api-gateway using spring cloud gateway

--- order-service/src/main/java/com/rizkiaazmi/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rizkiaazmi/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -41,8 +41,8 @@ public class OrderService {
                 .toList();
 
         // Call Inventory service, and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
