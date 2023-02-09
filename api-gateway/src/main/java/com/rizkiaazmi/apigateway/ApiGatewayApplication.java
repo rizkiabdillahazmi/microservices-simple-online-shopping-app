53964d90d8f205542f800ab3184f42e0ca55678c
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-02-09 13:52:07 +0700

    setup api gateway

---
+++ api-gateway/src/main/java/com/rizkiaazmi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.rizkiaazmi.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Rizki Abdillah Azmi on 09-Feb-23
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
