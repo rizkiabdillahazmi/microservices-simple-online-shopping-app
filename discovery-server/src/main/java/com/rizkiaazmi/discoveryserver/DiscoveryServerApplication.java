20a886e0d66a73187dfa933662c8348eaf0e9400
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-02-09 11:29:02 +0700

    basic config eureka server and client

---
+++ discovery-server/src/main/java/com/rizkiaazmi/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.rizkiaazmi.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Rizki Abdillah Azmi on 09-Feb-23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
