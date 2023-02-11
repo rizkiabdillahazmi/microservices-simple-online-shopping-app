33dbfd48bb3dd57ba5d69bfb820fe4db75f2a4ae
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-02-11 17:58:18 +0700

    service registry integration with consul

--- order-service/src/main/java/com/rizkiaazmi/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rizkiaazmi/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.rizkiaazmi.orderservice.controller;
 
 import com.rizkiaazmi.orderservice.dto.OrderRequest;
 import com.rizkiaazmi.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
